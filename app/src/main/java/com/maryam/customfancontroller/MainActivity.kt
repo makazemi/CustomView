2d5fe72bd34c08c6288c328864ffd7accf431ffa
mary8 <mary8shtr@gmail.com>
2020-07-21 15:36:12 +0430

    create drawing shader branch

--- app/src/main/java/com/maryam/customfancontroller/MainActivity.kt
+++ app/src/main/java/com/maryam/customfancontroller/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.maryam.customfancontroller
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(ClippedView(this))
+        setContentView(R.layout.activity_main)
+        val dialog = createInstructionsDialog()
+        dialog.show()
+    }
+    private fun createInstructionsDialog(): Dialog {
+        val builder = AlertDialog.Builder(this)
+        builder.setIcon(R.drawable.android)
+            .setTitle(R.string.instructions_title)
+            .setMessage(R.string.instructions)
+            .setPositiveButtonIcon(ContextCompat.getDrawable(this, android.R.drawable.ic_media_play))
+        return builder.create()
     }
 }
